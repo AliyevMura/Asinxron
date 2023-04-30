2bc60db79b36a73bbb3f38ffc62d5dad6d6a8312
AliyevMura <7lvghl9@code.edu.az>
2023-04-30 17:55:10 +0400

    Initial commit

---
+++ Models/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using Aspose.Cells.Drawing.Texts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asinxron.Models
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public int MaxBulletCount { get;  set; }
+        public List<Bullet> Bullets { get; set; } = new List<Bullet>();
+        public int BulletCount { get;  set; }
+        public string BulletType { get;  set; }
+
+        public Weapon(int maxBulletCount, string bulletType)
+        {
+            MaxBulletCount = maxBulletCount;
+            BulletCount = maxBulletCount;
+            BulletType = bulletType;
+           Bullets = new List<Bullet>();    
+        }
+
+        public void Fire()
+        {
+            if (BulletCount == 0)
+            {
+                Console.WriteLine("Gulle azalir!");
+            }
+            else
+            {
+                Console.WriteLine($"Atew {BulletType} gulle. {BulletCount} gulle qalib.");
+            }
+        }
+    }
+
+    public enum BulletType
+    {
+        Standard,
+        ArmorPiercing,
+        Incendiary
+    }
+    public void PullTrigger()
+    {
+        if (Bullets.Count == 0)
+        {
+            Console.WriteLine("Güllə qalmadı!");
+            return;
+        }
+
+        Console.WriteLine($"Növbəti güllə {Bullets[0].Type}.");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿#region Task1
+
+using Asinxron.Models;
+using System.Diagnostics;
+string[] urls = new string[]
+{
+    "https://docs.microsoft.com",
+    "https://docs.microsoft.com/aspnet/core",
+    "https://docs.microsoft.com/azure",
+    "https://docs.microsoft.com/azure/devops",
+    "https://docs.microsoft.com/dotnet",
+    "https://docs.microsoft.com/dynamics365",
+    "https://docs.microsoft.com/education",
+    "https://docs.microsoft.com/enterprise-mobility-security",
+    "https://docs.microsoft.com/gaming",
+    "https://docs.microsoft.com/graph",
+    "https://docs.microsoft.com/microsoft-365",
+    "https://docs.microsoft.com/office",
+    "https://docs.microsoft.com/powershell"
+};
+Asinxron
+GetHttpsContent(urls);
+
+//void GetHttpsContent(string[] urls)
+//{
+//    HttpClient client = new HttpClient();
+//    Stopwatch sw = Stopwatch.StartNew();
+//    sw.Start();
+//    foreach (string url in urls)
+//    {
+//        var result = client.GetStringAsync(url).Result;
+//        Console.WriteLine(result.Length);
+//    }
+//    sw.Stop();
+//    Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+//}
+
+
+//non-asinxron
+
+//await GetHttpsContentAsync(urls);
+//async Task GetHttpsContentAsync(string[] urls)
+//{
+
+//    HttpClient client = new HttpClient();
+//    List<Task<string>> tasks = new List<Task<string>>();
+//    Stopwatch sw = Stopwatch.StartNew();
+
+//    foreach (string url in urls)
+//    {
+//        var result = client.GetStringAsync(url);
+//        tasks.Add(result);
+
+//    }
+//    sw.Start();
+//    string[] arr = await Task.WhenAll(tasks);
+//    foreach (var item in arr)
+//    {
+//        Console.WriteLine(item.Length);
+//    }
+//    sw.Stop();
+//    Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+//}
+
+#endregion
+
+
+
+Weapon pistol = new Weapon(10, Standard);
+pistol.Fire();  
+
+Weapon sniperRifle = new Weapon(5, ArmorPiercing);
+sniperRifle.Fire();  
+Weapon shotgun = new Weapon(0, Incendiary);
+shotgun.Fire(); 
+
+
+
+
+
+
+
